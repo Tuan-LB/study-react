7199ce87b05578649f6d98ab05e4c3a4cd9d0f51
TuanLB <tuanlb2311@gmail.com>
2021-04-27 17:30:49 +0700

    study

--- src/containers/App.js
+++ src/containers/App.js
@@ -71,7 +71,8 @@ class App extends React.Component {
 
         return (
             <div className="app">
-                <Cockpit 
+                <Cockpit
+                    appTitle={this.props.title}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonHandler}/>
--- src/environment/progress.js
+++ src/environment/progress.js
@@ -1,6 +1,21 @@
-class Progress {
+import React from 'react';
 
-    progress = '7.4';
+class Progress extends React.Component {
+
+    progress = '7.5';
+
+    constructor() {} // hàm khởi tạo
+    componentWillMount = () => {} // trước khi re-render component
+    componentWillReceiveProps = () => {} //chạy khi component con nhập props từ cha
+    shouldComponentUpdate = () => {} // Nếu như return false thì các phương thực componentWillUpdate, render, componentDidUpdate sẽ không được chạy nữa
+    componentWillUpdate = () => {} // trước khi re-render component
+    componentDidUpdate = () => {} // sau khi re-render componet
+    componentDidCatch = () => {} // bắt lỗi
+    componentDidMount = () => {} //sử dụng AJAX để fetch những dữ liệu khởi tạo từ một API thì componentDidMount là nơi để thực hiện lời gọi AJAX đó
+    componentWillUnmount = () => {} //gọi trước khi một component bị remove khỏi một DOM giống destroy angular
+    render() {
+        return;
+    }
 
 }
 
